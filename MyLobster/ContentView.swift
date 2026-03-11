0676eb72186c3d92ac5635c80630854664166e5f
yveschao <njujack16@gmail.com>
2026-03-10 21:21:43 -0400

    Initial Commit

---
+++ MyLobster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyLobster
+//
+//  Created by Yves Chao on 3/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyLobster/MyLobsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLobsterApp.swift
+//  MyLobster
+//
+//  Created by Yves Chao on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct MyLobsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
